f3d464411aa04e35a492d9ee0cf09c20c232514c
Justin.W <Justin.Bj@msn.com>
2024-10-04 19:20:25 +0800

    feat:页面优化

--- config.js
+++ config.js
@@ -2,7 +2,7 @@ module.exports = {
 	VERSION: '1.0.0',
 	CRYPTO_KEY: 'DJTggiIeOBu3blSX',
 	CRYPTO_IV: '2oFtRtKzfnkxLB18',
-	//API_HOST: 'http://127.0.0.1:8000'
-	API_HOST: '//api.91gongshe.com'
+	API_HOST: 'http://127.0.0.1:8000'
+	//API_HOST: '//api.91gongshe.com'
 	
 }
